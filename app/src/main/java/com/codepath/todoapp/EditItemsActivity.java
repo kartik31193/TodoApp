2a8c65b8c9d15e0e7f1a494809dcd63f7fc34b29
Kartik Budholiya <kartik31193@gmail.com>
2017-08-19 23:10:45 +0530

    changes

--- app/src/main/java/com/codepath/todoapp/EditItemsActivity.java
+++ app/src/main/java/com/codepath/todoapp/EditItemsActivity.java
@@ -1,11 +1,13 @@
 package com.codepath.todoapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 
 public class EditItemsActivity extends AppCompatActivity {
 
@@ -24,6 +26,21 @@ public class EditItemsActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
+    }
+    // ActivityNamePrompt.java -- launched for a result
+    public void onSubmit(View v) {
+        EditText etName = (EditText) findViewById(R.id.editText);
+
+        // Prepare data intent
+        Intent data = new Intent();
+        // Pass relevant data back as a result
+        data.putExtra("editText", etName.getText().toString());
+       // data.putExtra("code", 200); // ints work too
+        // Activity finished ok, return the data
+        setResult(RESULT_OK, data); // set result code and bundle data for response
+        finish(); // closes the activity, pass data to parent
     }
 
 }
